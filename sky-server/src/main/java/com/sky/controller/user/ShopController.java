c9ff32500ef5eceafd75fc0ec84226658ff12950
wangyingxin <shintokyo2024@gmail.com>
2025-05-10 19:13:56 +0900

    feat: 添加用户端ShopController以获取店铺营业状态，优化Swagger配置

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,15 +1,17 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController("adminShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺管理")
 @RequiredArgsConstructor
 public class ShopController {
@@ -18,18 +20,6 @@ public class ShopController {
     private final RedisTemplate<String, String> redisTemplate;
     
     public static String KEY = "SHOP_STATUS";
-    
-    /**
-     * 设置店铺营业状态
-     *
-     * @param status 店铺状态
-     * @return Result<String>
-     */
-    @PutMapping("/{status}")
-    public Result<String> setStatus(@PathVariable Integer status) {
-        redisTemplate.opsForValue().set(KEY, String.valueOf(status));
-        return Result.success();
-    }
     
     /**
      * 获取店铺营业状态
